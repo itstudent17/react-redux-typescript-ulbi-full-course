303c7c55b6f73eeae195d6becf3847409568e58b
itstudent17 <itstudent17@outlook.com>
2021-05-21 15:09:48 +0300

    created rootReducer + Provider with store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
+import {rootReducer} from "./reducers";
 
-export const store = createStore(reducer, applyMiddleware(thunk))
+export const store = createStore(rootReducer, applyMiddleware(thunk))
